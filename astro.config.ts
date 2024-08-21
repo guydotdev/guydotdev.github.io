f38f557ecac820d0ba35c8269812906617476d78
Guy Royse <guy@guyroyse.com>
2024-08-20 21:59:23 -0400

    incremental

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    })
+  ],
   vite: {
     server: {
       watch: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ['./src/**/*.{astro,html,js,md,ts}'],
 
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['"Raleway"', ...defaultTheme.fontFamily.sans],
+        mono: ['"Fira Code"', ...defaultTheme.fontFamily.mono]
+      },
+      colors: {
+        'royse-red': '#a52a2a',
+        'programer-peach': '#ce9a5f',
+        'dorkness-rising': '#1a1921',
+        'not-white': '#fafafa'
+      }
+    }
   },
 
   plugins: []
