4f8b349d9d6d65d3c80489cfd59abfea7a4aa602
Guy Royse <guy@guyroyse.com>
2024-08-25 22:45:07 -0400

    added collection for blog

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { z, defineCollection } from 'astro:content'
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()).optional()
+  })
+})
+
+export const collections = {
+  blog: blogCollection
+}
